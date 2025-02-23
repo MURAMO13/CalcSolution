38298c46e40e4c31b0dc35fb8b368367f6866cc3
muramo13 <monbon08494@gmail.com>
2025-02-23 13:44:52 +0500

    Пришлось два проекта в одну папку.

---
+++ Calc/Calculator.cs
@@ -0,0 +1,30 @@
+﻿namespace Calc;
+/// <summary>
+/// Класс для математических операций
+/// </summary>
+public class Calculator
+    {
+        public int Additional(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Miltiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Division(int a, int b)
+        {
+            if (b == 0)
+                throw new Exception("На ноль делить нельзя!");
+            
+            return a / b;
+        }
+    }
+
---
+++ CalcTest/CalculatorTests.cs
@@ -0,0 +1,52 @@
+﻿using Calc;
+namespace CalcTest;
+/// <summary>
+/// Тесты для класса Calculator
+/// </summary>
+class CalculatorTests
+{
+
+    [Test]
+    public void AdditionalTest()
+    {
+        Calculator calculator = new Calculator();
+        int result = calculator.Additional(1, 2);
+        Assert.That(result, Is.EqualTo(3));
+    }
+
+    [Test]
+    public void SubtractionTest()
+    {
+        Calculator calculator = new Calculator();
+        int result = calculator.Subtraction(1, 2);
+        Assert.That(result, Is.EqualTo(-1));
+    }
+
+    [Test]
+    public void MiltiplicationTest()
+    {
+        Calculator calculator = new Calculator();
+        int result = calculator.Miltiplication(1, 2);
+        Assert.That(result, Is.EqualTo(2));
+    }
+    
+    [Test]
+    public void DivisionTest() 
+    {
+        Calculator calculator = new Calculator();
+        int result = calculator.Division(1, 2);
+        Assert.That(result, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void DivisionByZeroTest()
+    {
+        Calculator calculator = new Calculator();
+        
+        var ex = Assert.Throws<Exception>(() => calculator.Division(1, 0));
+
+        Assert.That(ex.Message, Is.EqualTo("На ноль делить нельзя!"));
+    }
+
+
+}
